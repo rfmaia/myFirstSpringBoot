7bd0ee14407fe516d36398328d7a8979a5829832
Rafael Maia <rafaelmaiadata@gmail.com>
2021-09-25 17:04:21 -0300

    H2 database

--- src/main/java/com/devsuperior/myfirstproject/resources/ProductResource.java
+++ src/main/java/com/devsuperior/myfirstproject/resources/ProductResource.java
@@ -27,7 +27,7 @@ public class ProductResource {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Product> findById(@PathVariable Long id) {
-		Product obj = productRepository.findById(id);
+		Product obj = productRepository.findById(id).get();
 		return ResponseEntity.ok().body(obj);
 	}
 }
